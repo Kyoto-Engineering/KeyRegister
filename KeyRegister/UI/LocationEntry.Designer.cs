e7eb5abb984012a2d159c87e2f7168ac453ed8bd
Ikramunnesa <ikraafi@gmail.com>
2017-04-18 12:04:33 +0600

    create KeyRegister/UI/LocationEntry.Designer.cs

---
+++ KeyRegister/UI/LocationEntry.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace KeyRegister.UI
+{
+    partial class LocationEntry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.locationNameText = new System.Windows.Forms.TextBox();
+            this.createButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(264, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Location Creation";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Location Name       :";
+            // 
+            // locationNameText
+            // 
+            this.locationNameText.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.locationNameText.Location = new System.Drawing.Point(197, 95);
+            this.locationNameText.Name = "locationNameText";
+            this.locationNameText.Size = new System.Drawing.Size(408, 24);
+            this.locationNameText.TabIndex = 2;
+            // 
+            // createButton
+            // 
+            this.createButton.BackColor = System.Drawing.Color.Plum;
+            this.createButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.createButton.Location = new System.Drawing.Point(310, 311);
+            this.createButton.Name = "createButton";
+            this.createButton.Size = new System.Drawing.Size(80, 39);
+            this.createButton.TabIndex = 3;
+            this.createButton.Text = "Create";
+            this.createButton.UseVisualStyleBackColor = false;
+            // 
+            // LocationEntry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(683, 401);
+            this.Controls.Add(this.createButton);
+            this.Controls.Add(this.locationNameText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "LocationEntry";
+            this.Text = "Location Create";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox locationNameText;
+        private System.Windows.Forms.Button createButton;
+    }
+}
