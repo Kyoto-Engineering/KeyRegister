fdb1e2e3cf4e3d7c9717d348add83a22450ad8bc
Ikramunnesa <ikraafi@gmail.com>
2017-04-18 13:18:21 +0600

    create KeyRegister/UI/KeyEntry.Designer.cs

---
+++ KeyRegister/UI/KeyEntry.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace KeyRegister.UI
+{
+    partial class KeyEntry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.createButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.propertyNameText = new System.Windows.Forms.TextBox();
+            this.keyTypeCombo = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // createButton
+            // 
+            this.createButton.BackColor = System.Drawing.Color.Plum;
+            this.createButton.ForeColor = System.Drawing.Color.Black;
+            this.createButton.Location = new System.Drawing.Point(310, 306);
+            this.createButton.Name = "createButton";
+            this.createButton.Size = new System.Drawing.Size(74, 40);
+            this.createButton.TabIndex = 0;
+            this.createButton.Text = "Create";
+            this.createButton.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(267, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Key Creation";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Property Name  :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Key Type          :";
+            // 
+            // propertyNameText
+            // 
+            this.propertyNameText.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.propertyNameText.Location = new System.Drawing.Point(166, 78);
+            this.propertyNameText.Name = "propertyNameText";
+            this.propertyNameText.Size = new System.Drawing.Size(293, 24);
+            this.propertyNameText.TabIndex = 4;
+            // 
+            // keyTypeCombo
+            // 
+            this.keyTypeCombo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.keyTypeCombo.FormattingEnabled = true;
+            this.keyTypeCombo.Location = new System.Drawing.Point(166, 120);
+            this.keyTypeCombo.Name = "keyTypeCombo";
+            this.keyTypeCombo.Size = new System.Drawing.Size(293, 26);
+            this.keyTypeCombo.TabIndex = 5;
+            // 
+            // KeyEntry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 395);
+            this.Controls.Add(this.keyTypeCombo);
+            this.Controls.Add(this.propertyNameText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.createButton);
+            this.Name = "KeyEntry";
+            this.Text = "Key Create";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button createButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox propertyNameText;
+        private System.Windows.Forms.ComboBox keyTypeCombo;
+    }
+}
