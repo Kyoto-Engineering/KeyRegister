589dcb3b9fd03aa630a527fdc4e3ffeefd128934
Ikramunnesa <ikraafi@gmail.com>
2017-04-18 14:21:18 +0600

    create KeyRegister/UI/KeyAllocationEntry.Designer.cs

---
+++ KeyRegister/UI/KeyAllocationEntry.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace KeyRegister.UI
+{
+    partial class KeyAllocationEntry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.territoryNameText = new System.Windows.Forms.TextBox();
+            this.propertyNameText = new System.Windows.Forms.TextBox();
+            this.locationNameText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // saveButton
+            // 
+            this.saveButton.BackColor = System.Drawing.Color.Plum;
+            this.saveButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.saveButton.Location = new System.Drawing.Point(263, 344);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 44);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(279, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Key Allocation";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 18);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Territory Name       :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(161, 18);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Property Name       :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 18);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Location Name       :";
+            // 
+            // territoryNameText
+            // 
+            this.territoryNameText.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.territoryNameText.Location = new System.Drawing.Point(175, 70);
+            this.territoryNameText.Name = "territoryNameText";
+            this.territoryNameText.Size = new System.Drawing.Size(413, 24);
+            this.territoryNameText.TabIndex = 1;
+            // 
+            // propertyNameText
+            // 
+            this.propertyNameText.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.propertyNameText.Location = new System.Drawing.Point(175, 121);
+            this.propertyNameText.Name = "propertyNameText";
+            this.propertyNameText.Size = new System.Drawing.Size(413, 24);
+            this.propertyNameText.TabIndex = 2;
+            // 
+            // locationNameText
+            // 
+            this.locationNameText.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.locationNameText.Location = new System.Drawing.Point(175, 185);
+            this.locationNameText.Name = "locationNameText";
+            this.locationNameText.Size = new System.Drawing.Size(413, 24);
+            this.locationNameText.TabIndex = 3;
+            // 
+            // KeyAllocationEntry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 422);
+            this.Controls.Add(this.locationNameText);
+            this.Controls.Add(this.propertyNameText);
+            this.Controls.Add(this.territoryNameText);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.saveButton);
+            this.Name = "KeyAllocationEntry";
+            this.Text = "Key Allocation";
+            this.Load += new System.EventHandler(this.KeyAllocationEntry_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox territoryNameText;
+        private System.Windows.Forms.TextBox propertyNameText;
+        private System.Windows.Forms.TextBox locationNameText;
+    }
+}
