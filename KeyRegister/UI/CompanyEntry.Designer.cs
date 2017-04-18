6ce470ef592c5d3582c515027c47f697a48c0469
Ikramunnesa <ikraafi@gmail.com>
2017-04-18 11:02:25 +0600

    initial commit

---
+++ KeyRegister/UI/CompanyEntry.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace KeyRegister.UI
+{
+    partial class CompanyEntry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.companyNameText = new System.Windows.Forms.TextBox();
+            this.createButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(283, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Company Creation";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(176, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Company Name     :";
+            // 
+            // companyNameText
+            // 
+            this.companyNameText.Location = new System.Drawing.Point(191, 71);
+            this.companyNameText.Multiline = true;
+            this.companyNameText.Name = "companyNameText";
+            this.companyNameText.Size = new System.Drawing.Size(470, 30);
+            this.companyNameText.TabIndex = 2;
+            // 
+            // createButton
+            // 
+            this.createButton.BackColor = System.Drawing.Color.Plum;
+            this.createButton.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.createButton.Location = new System.Drawing.Point(384, 279);
+            this.createButton.Name = "createButton";
+            this.createButton.Size = new System.Drawing.Size(117, 59);
+            this.createButton.TabIndex = 3;
+            this.createButton.Text = "Create";
+            this.createButton.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 401);
+            this.Controls.Add(this.createButton);
+            this.Controls.Add(this.companyNameText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Company Entry";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox companyNameText;
+        private System.Windows.Forms.Button createButton;
+    }
+}
+
