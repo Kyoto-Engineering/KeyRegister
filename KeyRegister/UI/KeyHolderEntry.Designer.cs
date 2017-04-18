14aa6c9112b0b5837b67239572474012092735a4
Ikramunnesa <ikraafi@gmail.com>
2017-04-18 12:17:05 +0600

    create new form

---
+++ KeyRegister/UI/KeyHolderEntry.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace KeyRegister.UI
+{
+    partial class KeyHolderEntry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.keyHolderNameText = new System.Windows.Forms.TextBox();
+            this.locationNameText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // saveButton
+            // 
+            this.saveButton.BackColor = System.Drawing.Color.Plum;
+            this.saveButton.Location = new System.Drawing.Point(314, 328);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(84, 41);
+            this.saveButton.TabIndex = 0;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(259, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Key Holder Creation";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(222, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Key Holder Name          :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(219, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Location Name             :";
+            // 
+            // keyHolderNameText
+            // 
+            this.keyHolderNameText.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.keyHolderNameText.Location = new System.Drawing.Point(228, 83);
+            this.keyHolderNameText.Name = "keyHolderNameText";
+            this.keyHolderNameText.Size = new System.Drawing.Size(385, 24);
+            this.keyHolderNameText.TabIndex = 4;
+            this.keyHolderNameText.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // locationNameText
+            // 
+            this.locationNameText.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.locationNameText.Location = new System.Drawing.Point(228, 136);
+            this.locationNameText.Name = "locationNameText";
+            this.locationNameText.Size = new System.Drawing.Size(385, 24);
+            this.locationNameText.TabIndex = 5;
+            // 
+            // KeyHolderEntry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 410);
+            this.Controls.Add(this.locationNameText);
+            this.Controls.Add(this.keyHolderNameText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.saveButton);
+            this.Name = "KeyHolderEntry";
+            this.Text = "Key Holder Create";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox keyHolderNameText;
+        private System.Windows.Forms.TextBox locationNameText;
+    }
+}
