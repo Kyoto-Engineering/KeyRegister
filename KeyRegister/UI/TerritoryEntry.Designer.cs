020bf73e69f7daa911aef2f7e8ab5cbf57a38c47
Ikramunnesa <ikraafi@gmail.com>
2017-04-18 11:55:03 +0600

    adding new ui

---
+++ KeyRegister/UI/TerritoryEntry.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace KeyRegister.UI
+{
+    partial class TerritoryEntry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.createButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.territoryNameText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // createButton
+            // 
+            this.createButton.BackColor = System.Drawing.Color.Plum;
+            this.createButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.createButton.Location = new System.Drawing.Point(309, 309);
+            this.createButton.Name = "createButton";
+            this.createButton.Size = new System.Drawing.Size(78, 45);
+            this.createButton.TabIndex = 0;
+            this.createButton.Text = "Create";
+            this.createButton.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Territory Name       :";
+            // 
+            // territoryNameText
+            // 
+            this.territoryNameText.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.territoryNameText.Location = new System.Drawing.Point(195, 81);
+            this.territoryNameText.Name = "territoryNameText";
+            this.territoryNameText.Size = new System.Drawing.Size(389, 24);
+            this.territoryNameText.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(273, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(197, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Territory Creation";
+            // 
+            // TerritoryEntry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 411);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.territoryNameText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.createButton);
+            this.Name = "TerritoryEntry";
+            this.Text = "Territory Creation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button createButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox territoryNameText;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ KeyRegister/UI/TerritoryEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyRegister.UI
+{
+    public partial class TerritoryEntry : Form
+    {
+        public TerritoryEntry()
+        {
+            InitializeComponent();
+        }
+    }
+}
