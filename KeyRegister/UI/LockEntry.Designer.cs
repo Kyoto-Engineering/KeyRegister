91fe0cccf14aef4615c18404ffa295758233d9e4
Ikramunnesa <ikraafi@gmail.com>
2017-04-18 15:14:36 +0600

    create KeyRegister/UI/LockEntry.Designer.cs

---
+++ KeyRegister/UI/LockEntry.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace KeyRegister.UI
+{
+    partial class LockEntry
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.createButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lockNameText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // createButton
+            // 
+            this.createButton.BackColor = System.Drawing.Color.Plum;
+            this.createButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.createButton.Location = new System.Drawing.Point(278, 259);
+            this.createButton.Name = "createButton";
+            this.createButton.Size = new System.Drawing.Size(96, 44);
+            this.createButton.TabIndex = 0;
+            this.createButton.Text = "Create";
+            this.createButton.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(240, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Lock Creation";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Lock Name    :";
+            // 
+            // lockNameText
+            // 
+            this.lockNameText.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lockNameText.Location = new System.Drawing.Point(148, 66);
+            this.lockNameText.Name = "lockNameText";
+            this.lockNameText.Size = new System.Drawing.Size(394, 24);
+            this.lockNameText.TabIndex = 3;
+            // 
+            // LockEntry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 381);
+            this.Controls.Add(this.lockNameText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.createButton);
+            this.Name = "LockEntry";
+            this.Text = "Lock Create";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button createButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox lockNameText;
+    }
+}
