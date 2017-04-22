5c9a77fb2455f237234126d60b9c0c994dc0dffc
asraf16a <siddique_iceiu@yahoo.com>
2017-04-22 17:19:50 +0600

    last

---
+++ KeyRegister/DAO/OverSeasAddress.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyRegister.DAO
+{
+  public   class OverSeasAddress
+    {
+        private int overSeasAddressId;
+        private string street;
+        private string state;
+        private string postalCode;
+
+        public int ForeignAddressId
+        {
+            get { return overSeasAddressId; }
+            set { overSeasAddressId = value; }
+        }
+
+        public string Street
+        {
+            get { return street; }
+            set { street = value; }
+        }
+
+        public string State
+        {
+            get { return state; }
+            set { state = value; }
+        }
+
+        public string PostalCode
+        {
+            get { return postalCode; }
+            set { postalCode = value; }
+        }
+    }
+}
