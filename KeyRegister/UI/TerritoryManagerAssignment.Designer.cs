55b1dc95d2265f2108fdd73c5fc9a8cf92ee9adc
Ikramunnesa <ikraafi@gmail.com>
2017-04-27 16:54:50 +0600

    create new form

---
+++ KeyRegister/UI/TerritoryManagerAssignment.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace KeyRegister.UI
+{
+    partial class TerritoryManagerAssignment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.territoryManagerNameText = new System.Windows.Forms.TextBox();
+            this.territoryNameCombo = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // saveButton
+            // 
+            this.saveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveButton.ForeColor = System.Drawing.Color.Plum;
+            this.saveButton.Location = new System.Drawing.Point(479, 295);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(100, 45);
+            this.saveButton.TabIndex = 0;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(248, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(331, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Territory Manager Assignment";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(267, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Territory Manager Name        :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(267, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Territory Name                     :";
+            // 
+            // territoryManagerNameText
+            // 
+            this.territoryManagerNameText.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.territoryManagerNameText.Location = new System.Drawing.Point(282, 79);
+            this.territoryManagerNameText.Name = "territoryManagerNameText";
+            this.territoryManagerNameText.Size = new System.Drawing.Size(276, 24);
+            this.territoryManagerNameText.TabIndex = 4;
+            // 
+            // territoryNameCombo
+            // 
+            this.territoryNameCombo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.territoryNameCombo.FormattingEnabled = true;
+            this.territoryNameCombo.Location = new System.Drawing.Point(282, 128);
+            this.territoryNameCombo.Name = "territoryNameCombo";
+            this.territoryNameCombo.Size = new System.Drawing.Size(276, 26);
+            this.territoryNameCombo.TabIndex = 5;
+            // 
+            // TerritoryManagerAssignment
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 395);
+            this.Controls.Add(this.territoryNameCombo);
+            this.Controls.Add(this.territoryManagerNameText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.saveButton);
+            this.Name = "TerritoryManagerAssignment";
+            this.Text = "TerritoryManagerAssignment";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox territoryManagerNameText;
+        private System.Windows.Forms.ComboBox territoryNameCombo;
+    }
+}
