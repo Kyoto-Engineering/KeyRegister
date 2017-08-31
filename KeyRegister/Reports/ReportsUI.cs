b900091bcc6f6ffadac9a145c9b9319c7c1e5bf0
Ikramunnesa <ikraafi@gmail.com>
2017-08-31 17:34:42 +0600

    key holder list report added

--- KeyRegister/Reports/ReportsUI.cs
+++ KeyRegister/Reports/ReportsUI.cs
@@ -176,5 +176,35 @@ namespace KeyRegister.Reports
             MainUI frm = new MainUI();
             frm.Show();
         }
+
+        private void KeyHolderListButton_Click(object sender, EventArgs e)
+        {
+            ReportViewer f2 = new ReportViewer();
+            TableLogOnInfos reportLogonInfos = new TableLogOnInfos();
+            TableLogOnInfo reportLogonInfo = new TableLogOnInfo();
+            ConnectionInfo reportConInfo = new ConnectionInfo();
+            Tables tables = default(Tables);
+            //	Table table = default(Table);
+            var with1 = reportConInfo;
+            with1.ServerName = "tcp:KyotoServer,49172";
+            with1.DatabaseName = "KeyRegistar";
+            with1.UserID = "sa";
+            with1.Password = "SystemAdministrator";
+            KeyHolderListReport cr = new KeyHolderListReport();
+            tables = cr.Database.Tables;
+            foreach (Table table in tables)
+            {
+                reportLogonInfo = table.LogOnInfo;
+                reportLogonInfo.ConnectionInfo = reportConInfo;
+                table.ApplyLogOnInfo(reportLogonInfo);
+            }
+            //f2.crystalReportViewer1.ParameterFieldInfo = paramFields;
+            //set the parameterfield information in the crystal report
+            f2.crystalReportViewer1.ReportSource = cr;
+            this.Visible = false;
+
+            f2.ShowDialog();
+            this.Visible = true;
+        }
     }
 }
